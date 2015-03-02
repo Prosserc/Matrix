6cac66333c936babd6dfcb6e74b1ecb61ef2f598
Chris Prosser <prosser_chris@yahoo.co.uk>
2015-03-02 17:57:34 +0000

    Tests - expanded

--- Tests/TestRunner.cs
+++ Tests/TestRunner.cs
@@ -1,15 +1,21 @@
 ﻿namespace Matrix.Tests
 {
     class TestRunner
-    {        public TestRunner()
+    {   
+        public TestRunner()
         {
+            // test multiplication result
             new MatrixMultiplicationResultTest();
+
+            // test indexing
             new MatrixIndexerTest();
 
             // test larger multiplication
             new MatrixMultiplicationTimeTest(100);
-            new MatrixMultiplicationTimeTest(500);
-            //new MatrixMultiplicationTest(10000, 200, 200, 10000); // test none square array
+            new MatrixMultiplicationTimeTest(250);
+            new MatrixMultiplicationTimeTest(1000);
+            new MatrixMultiplicationTimeTest(10000, 200, 200, 10000); // test none square array
+            new MatrixMultiplicationTimeTest(10000);
         }
     }
 }
